42f5e3d16ec694ea8e6819b68453573fa874d21a
Paul Werner <contact@paulwerner.net>
2022-05-18 23:54:59 +0200

    add db constraints

--- ops.migrations/000001_init.up.sql
+++ ops.migrations/000001_init.up.sql
@@ -11,18 +11,20 @@ CREATE TABLE IF NOT EXISTS accounts(
     description VARCHAR,
     type VARCHAR NOT NULL,
     balance_value BIGINT NOT NULL,
-    balance_currency VARCHAR NOT NULL
+    balance_currency VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT uc_accounts_user_id_name UNIQUE(user_id, name)
 );
 
 CREATE INDEX idx_accounts_id_user_id ON accounts(id, user_id);
 
-CREATE INDEX idx_accounts_user_id_name ON accounts(id, user_id);
-
 CREATE TABLE IF NOT EXISTS transactions(
     id VARCHAR PRIMARY KEY NOT NULL,
     account_id VARCHAR NOT NULL,
+    description VARCHAR,
     amount BIGINT NOT NULL,
-    currency VARCHAR NOT NULL
+    currency VARCHAR NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
 
 CREATE INDEX idx_transactions_id_account_id ON transactions(id, account_id);
