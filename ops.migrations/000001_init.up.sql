37593c87e4c400add6bf1e805b319352e8ca0005
Paul Werner <contact@paulwerner.net>
2022-05-18 23:28:29 +0200

    remove constraints from db

--- ops.migrations/000001_init.up.sql
+++ ops.migrations/000001_init.up.sql
@@ -10,20 +10,19 @@ CREATE TABLE IF NOT EXISTS accounts(
     name VARCHAR NOT NULL,
     description VARCHAR,
     type VARCHAR NOT NULL,
-    balance_amount BIGINT NOT NULL,
-    currency VARCHAR NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT uc_accounts_user_id_name UNIQUE(user_id, name)
+    balance_value BIGINT NOT NULL,
+    balance_currency VARCHAR NOT NULL
 );
 
 CREATE INDEX idx_accounts_id_user_id ON accounts(id, user_id);
+
+CREATE INDEX idx_accounts_user_id_name ON accounts(id, user_id);
 
 CREATE TABLE IF NOT EXISTS transactions(
     id VARCHAR PRIMARY KEY NOT NULL,
     account_id VARCHAR NOT NULL,
     amount BIGINT NOT NULL,
-    currency VARCHAR NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES accounts(id)
+    currency VARCHAR NOT NULL
 );
 
 CREATE INDEX idx_transactions_id_account_id ON transactions(id, account_id);
