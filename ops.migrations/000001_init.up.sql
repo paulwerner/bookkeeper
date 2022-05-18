17a80c1dac3c1e2fe625b1de4542f2317b45653f
Paul Werner <contact@paulwerner.net>
2022-05-18 14:42:42 +0200

    add migration scripts

---
+++ ops.migrations/000001_init.up.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS users(
+    id VARCHAR PRIMARY KEY NOT NULL,
+    name VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accounts(
+    id VARCHAR PRIMARY KEY NOT NULL,
+    user_id VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    description VARCHAR,
+    type VARCHAR NOT NULL,
+    balance_amount BIGINT NOT NULL,
+    currency VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT uc_accounts_user_id_name UNIQUE(user_id, name)
+);
+
+CREATE INDEX idx_accounts_id_user_id ON accounts(id, user_id);
+
+CREATE TABLE IF NOT EXISTS transactions(
+    id VARCHAR PRIMARY KEY NOT NULL,
+    account_id VARCHAR NOT NULL,
+    amount BIGINT NOT NULL,
+    currency VARCHAR NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+CREATE INDEX idx_transactions_id_account_id ON transactions(id, account_id);
