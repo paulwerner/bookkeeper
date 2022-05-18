cf9442e297d6e735af2053ed3f96b649f46c518b
Paul Werner <contact@paulwerner.net>
2022-05-18 21:08:25 +0200

    add postgresql database setup

---
+++ infra/postgresql.go
@@ -0,0 +1,21 @@
+package infra
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func SetupPostgreSQLDatabase(host, port, user, password, dbname, sslmode string) *sql.DB {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
