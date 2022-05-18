cd4cf2814b36e3e7182deb12e45b20d055110692
Paul Werner <contact@paulwerner.net>
2022-05-18 22:23:16 +0200

    run migrations during test

--- store/store_test.go
+++ store/store_test.go
@@ -1,32 +1,51 @@
-package store_test
+package store
 
 import (
 	"context"
 	"database/sql"
 	"log"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/paulwerner/bookkeeper/infra"
+	"github.com/paulwerner/bookkeeper/ops"
 )
 
 var db *sql.DB
 
+
+func setWorkingDir() {
+	wd, _ := os.Getwd()
+	// go 1 directory up for migrations to be found
+	os.Chdir(filepath.Dir(wd))
+}
+
 func TestMain(m *testing.M) {
+	setWorkingDir()
 	ctx := context.Background()
 	container, database, err := infra.CreatePostgresTestContainer(ctx, "testdb")
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	err = ops.RunMigrations(database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	db = database
 	errCode := m.Run()
+
 	err = database.Close()
 	if err != nil {
 		log.Printf("failed to close db connection: %s", err)
 	}
+	
 	err = container.Terminate(ctx)
 	if err != nil {
 		log.Printf("failed to terminate the tet container: %s", err)
 	}
+	
 	os.Exit(errCode)
 }
