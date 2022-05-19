8ff9eac28ff376c62e579e89b1bdc6d6c418c0df
Paul Werner <contact@paulwerner.net>
2022-05-19 15:39:12 +0200

    extract jwt utils

--- security/authHandler.go
+++ security/authHandler.go
@@ -1,16 +1,11 @@
 package security
 
 import (
-	"os"
-	"time"
-
-	"github.com/golang-jwt/jwt/v4"
 	d "github.com/paulwerner/bookkeeper/domain"
 	"github.com/paulwerner/bookkeeper/uc"
+	"github.com/paulwerner/bookkeeper/utils"
 	"golang.org/x/crypto/bcrypt"
 )
-
-var tokenTimeToLive = 2 * time.Hour
 
 type authHandler struct{}
 
@@ -34,41 +29,10 @@ func (*authHandler) EncryptPassword(password string) (string, error) {
 	return string(hash), nil
 }
 
-func (*authHandler) GenUserToken(id d.UserID) (token string, err error) {
-	token, err = jwt.
-		NewWithClaims(jwt.SigningMethodHS256, newUserClaims(id, tokenTimeToLive)).
-		SignedString([]byte(os.Getenv("JWT_SECRET")))
-	return
+func (*authHandler) GenUserToken(id d.UserID) (string, error) {
+	return utils.GenUserToken(id)
 }
 
-func (*authHandler) GetUserID(t string) (d.UserID, error) {
-	token, err := jwt.ParseWithClaims(
-		t,
-		&userClaims{},
-		func(token *jwt.Token) (any, error) {
-			return []byte(os.Getenv("JWT_SECRET")), nil
-		},
-	)
-	if err != nil {
-		return "", err
-	}
-	if claims, ok := token.Claims.(*userClaims); ok && token.Valid {
-		return claims.ID, nil
-	}
-	return "", d.ErrInternalError
-}
-
-type userClaims struct {
-	ID d.UserID
-	jwt.StandardClaims
-}
-
-func newUserClaims(id d.UserID, ttl time.Duration) *userClaims {
-	return &userClaims{
-		ID: id,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(ttl).Unix(),
-			Issuer:    "bookkeeper-api",
-		},
-	}
+func (*authHandler) GetUserID(token string) (d.UserID, error) {
+	return utils.GetUserIDFromToken(token)
 }
---
+++ utils/jwt.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	d "github.com/paulwerner/bookkeeper/domain"
+)
+
+var tokenTimeToLive = 2 * time.Hour
+
+func GenUserToken(id d.UserID) (token string, err error) {
+	token, err = jwt.
+		NewWithClaims(jwt.SigningMethodHS256, newUserClaims(id, tokenTimeToLive)).
+		SignedString([]byte(os.Getenv("JWT_SECRET")))
+	return
+}
+
+func GetUserIDFromToken(t string) (d.UserID, error) {
+	token, err := jwt.ParseWithClaims(
+		t,
+		&userClaims{},
+		func(token *jwt.Token) (any, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*userClaims); ok && token.Valid {
+		return claims.ID, nil
+	}
+	return "", d.ErrInternalError
+}
+
+type userClaims struct {
+	ID d.UserID
+	jwt.RegisteredClaims
+}
+
+func newUserClaims(id d.UserID, ttl time.Duration) *userClaims {
+	return &userClaims{
+		ID: id,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(ttl)),
+			Issuer:    "bookkeeper-api",
+		},
+	}
+}
